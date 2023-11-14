ac3dcc87a865c4c1df2c4964ff0de1b0e0a58050
Emeka Umejiofor <pumej1985@gmail.com>
2023-11-14 00:46:24 -0500

    added files for terraform deploy

---
+++ ecs_main.tf
@@ -0,0 +1,70 @@
+# Define the provider for AWS
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create an ECS cluster
+resource "aws_ecs_cluster" "pumej_cluster" {
+  name = "amazon-ecs-cluster"
+}
+
+# Create a task definition
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family                   = "my-task-family-test"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = <<EOF
+[
+  {
+    "name": "pumej-container",
+    "image": "598189530267.dkr.ecr.ap-south-1.amazonaws.com/pumejrepo:v1.0",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOF
+}
+
+# Create a service to run the task on the cluster
+resource "aws_ecs_service" "my_service" {
+  name            = "amazon-service"
+  cluster         = aws_ecs_cluster.pumej_cluster.id
+  task_definition = aws_ecs_task_definition.my_task_definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = ["subnet-0f3c3846f9c0551b6"]
+    security_groups  = ["sg-08849123726c14ea7"]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
